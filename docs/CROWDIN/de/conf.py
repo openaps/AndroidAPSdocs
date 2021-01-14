9281e6fa54f1bcceb1213a05c74b3b1e22e7135b
andries-smit <mr.andries.smit@gmail.com>
2021-01-14 12:55:38 +0100

    Plit conf for each language

---
+++ docs/CROWDIN/de/conf.py
@@ -0,0 +1,20 @@
+import os
+
+exec (open("../../conf.py").read())
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+#html_logo = None
+html_logo = '../../drawing.png'
+
+# The name of an image file (relative to this directory) to use as a favicon of
+# the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#html_favicon = None
+html_favicon = '../../favicon.ico'
+
+# Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+html_search_language = 'de'
