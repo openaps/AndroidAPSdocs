28b302115c378a41aede7d8cc53dd5affa8ac230
peterleimbach <peterleimbach@icloud.com>
2023-01-22 20:23:59 +0100

    enable myst parser

--- docs/shared.conf.py
+++ docs/shared.conf.py
@@ -20,8 +20,8 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 
 
-from recommonmark.parser import CommonMarkParser
-from recommonmark.transform import AutoStructify
+#from recommonmark.parser import CommonMarkParser
+#from recommonmark.transform import AutoStructify
 import alabaster
 
 # -- General configuration ------------------------------------------------
@@ -39,7 +39,12 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
+    'myst_parser',
     # 'alabaster',
+]
+
+myst_enable_extensions = [
+  "colon_fence",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -124,7 +129,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default`
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
